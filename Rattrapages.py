4ff2320c4dc662aee814f89ac8edcbeb97fb1d33
RaslenDerbal <86241644+RaslenDerbal@users.noreply.github.com>
2022-04-15 17:02:40 +0200

    Update Rattrapages.py

--- Rattrapages.py
+++ Rattrapages.py
@@ -5,6 +5,7 @@ import numpy as np
 import streamlit as st 
 import pandas as pd
 import plotly.figure_factory as ff
+import plotly.express as px
 import matplotlib
 import matplotlib.pyplot as plt
 import time
@@ -13,21 +14,47 @@ import os
 
 #Opening and loading the data
 @st.cache
-def open_data():
-    return pd.read_csv('data_vis.csv')
+def open_data(nrows):
+    return pd.read_csv('data_vis.csv',nrows=nrows)
 
 
 #Settings of the visualisation
 st.set_page_config(page_title="Data Visualisation Rattrapages", layout="centered", initial_sidebar_state="expanded")
 
 #Open the data
-agri = open_data()
+agri = open_data(50)
 
 
 
 #Plot the data in multiple forms
 
-#Adding the filters on thhe dataset
+#Adding the filters on the dataset
 st.sidebar.header('Choose what to show')
-categories_count = agri['GRP_CULTU'].value.counts()
-categories = st.sidebar.slider("Cultures", int(categories_count.min()), int(categories_count.max()), (int(categories_count.min()), int(categories_count.max())), 1)
+slide = st.slider('Linear graph or Map graph here :',min_value= 0 , max_value= 2)
+graph = st.sidebar.selectbox(label ="Choose what graphh to use",options=['Pie','Bar'])
+colonnes =df.select_dtypes(['float','int']).columns
+
+#Coordonates for the bar
+scat1 = {"":"","Culture Code" : agri['CODE_CULTU'],,"Culture Library" : agri['LBL_CULTU']}
+scat2 = {"":"","Surface" : agri['SURFACE_HA']}
+
+#Add the figures
+@measure
+def figure(agri):
+    
+    #Pie chart
+    if graph == 'Pie':
+        camembert = px.pie(agri, values='GRP_CULTU', names='Cultures', color_discrete_sequence=px.colors.sequential.RdBu)
+        camembert.show()
+        
+    #Bar chart
+    if graph == 'Bar':
+        st.sidebar.subheader('Choose values to show')
+        abscissa = st.sidebar.selectbox('Abscissa', options=scat1)
+        ordonate =  st.sidebar.selectbox('Ordonate', options=scat2)
+        barre = px.scatter(agri, x=abscissa ,y=ordonate, color_discrete_sequence=px.colors.sequential.RdBu)
+        barre.show()
+        
+        
+        
+        
