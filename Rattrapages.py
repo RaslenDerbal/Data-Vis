9afc8bbd8dc3556f05d6fc6ae8915a05216e4a83
RaslenDerbal <86241644+RaslenDerbal@users.noreply.github.com>
2022-04-15 15:30:39 +0200

    Add files via upload

---
+++ Rattrapages.py
@@ -0,0 +1,26 @@
+
+#All the imports to do
+
+import numpy as np
+import streamlit as st 
+import pandas as pd
+import plotly.figure_factory as ff
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+import seaborn as sns
+import os
+
+#Opening and loading the data
+@st.cache
+def open_data():
+    return pd.read_csv('C:\data_vis.csv')
+
+
+#Settings of the visualisation
+st.set_page_config(page_title="Data Visualisation Rattrapages", layout="centered", initial_sidebar_state="expanded")
+
+#Open the data
+agri = open_data()
+
+st.write(agri)
